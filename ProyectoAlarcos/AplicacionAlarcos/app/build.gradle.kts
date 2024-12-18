9145dd98ca97e40880faf3a9fbe323b3114c4e5a
vod275 <vod27577@hotmail.com>
2024-12-18 11:28:50 +0100

    trabajo

--- ProyectoAlarcos/AplicacionAlarcos/app/build.gradle.kts
+++ ProyectoAlarcos/AplicacionAlarcos/app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
